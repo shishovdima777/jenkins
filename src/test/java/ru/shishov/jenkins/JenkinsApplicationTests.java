c0c219abe381c4c38bfa52c94fdb7420d5d1f430
Dima <shishovdima777@gmail.com>
2024-05-19 14:34:54 +0300

    first local commit

---
+++ src/test/java/ru/shishov/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shishov.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
